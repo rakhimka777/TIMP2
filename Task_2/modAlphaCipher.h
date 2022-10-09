4c3c19d28294b0859e525891937abe80e74b5ad9
rakhimka777 <113301356+rakhimka777@users.noreply.github.com>
2022-10-09 19:22:47 +0400

    Update modAlphaCipher.h

--- Task_2/modAlphaCipher.h
+++ Task_2/modAlphaCipher.h
@@ -1,3 +1,4 @@
+//modAlphaCipher.h
 #pragma once
 #include <iostream>
 #include <string>
@@ -12,10 +13,19 @@ using namespace std;
 class modAlphaCipher
 {
 private:
-    int newkey; // количество столбцов таблицы
+    int key;
+    string getValidText(const std::string & s);
 public:
     modAlphaCipher()=delete;
-    modAlphaCipher(const int& key) :newkey(key) {};   // конструктор
+    modAlphaCipher(const int& newkey) :key(newkey) {};   // конструктор
     string coder(const string& open_st);   // зашифрование
-    string decoder(const string& cipher_st); // расшифрование 
+    string decoder(const string& cipher_st); // расшифрование
+};
+class cipher_error: public std::invalid_argument
+{
+public:
+    explicit cipher_error (const std::string& what_arg):
+        std::invalid_argument(what_arg) {}
+    explicit cipher_error (const char* what_arg):
+        std::invalid_argument(what_arg) {}
 };
