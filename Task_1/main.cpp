3ce2c2e9fe78b1e9c5c354172f31ce6a35640cd5
rakhimka777 <113301356+rakhimka777@users.noreply.github.com>
2022-10-09 19:17:17 +0400

    Update main.cpp

--- Task_1/main.cpp
+++ Task_1/main.cpp
@@ -1,52 +1,31 @@
+//main.cpp
 #include "modAlphaCipher.h"
-bool isValid(const string& s)
+
+void check(const string& Text, const string& key)
 {
-    std::locale loc("ru_RU.UTF-8");
-    std::wstring_convert<std::codecvt_utf8<wchar_t>, wchar_t> codec;
-    std::wstring ws = codec.from_bytes(s);
-    std::string numAlpha = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
-    std::wstring wA = codec.from_bytes(numAlpha);
-    for(unsigned int i = 0; i < ws.size(); i++) {
-        if (wA.find(ws[i]) == string::npos) {
-            return false;
-        }
-    }
-    return true;
+    string cipherText;
+    string decryptedText;
+    modAlphaCipher cipher(key);
+    cipherText = cipher.encrypt(Text);
+    decryptedText = cipher.decrypt(cipherText);
+    cout<<"key="<<key<<endl;
+    cout<<Text<<endl;
+    cout<<cipherText<<endl;
+    cout<<decryptedText<<endl;
+    if (Text == decryptedText)
+        cout<<"Ok\n";
+    else
+        cout<<"Err\n";
 }
 int main(int argc, char **argv)
 {
-    string key;
-    string text;
-    unsigned op;
-    cout<<"Cipher ready. Input key: ";
-    cin>>key;
-    if (!isValid(key)) {
-        cerr<<"key not valid\n";
-        return 1;
-    }
-    cout<<"Key loaded\n";
-    modAlphaCipher cipher(key);
-    do {
-        cout<<"Cipher ready. Input operation (0-exit, 1-encrypt, 2-decrypt): ";
-        cin>>op;
-        if (op > 2) {
-            cout<<"Illegal operation\n";
-        } else if (op >0) {
-            cout<<"Cipher ready. Input text: ";
-            cin>>text;
-            if (isValid(text)) {
-                if (op==1) {
-                    cout<<"Encrypted text: "<<cipher.encrypt(text)<<endl;
-                } else {
-                    cout<<"Decrypted text: "<<cipher.decrypt(text)<<endl;
-                }
-            } else {
-                cout<<"Operation aborted: invalid text\n";
-
-            }
-        }
-    } while (op!=0);
-    
-    return 0;
-    Console.ReadLine();
+    check("СПАСИСОХРАНИОТСЕССИИ","Спасу");
+    check("СПАСИСОХРАНИОТСЕССИИ","СПАСУ");
+    check("СПАСИСОХРАНИОТСЕССИИ","!СПАСУ!");
+    check("СПАСИ СОХРАНИ ОТ СЕССИИ","11032002");
+    check("1234567890","NO");
+    check("СПАСИСОХРАНИОТСЕССИИ","НЕ СПАСУ");
+    check("123456789","СПАСУ");
+    check("НЕЗАДАВАЙТЕ КОНТРОЛЬНЫХВОПРОСОВ","АБВГ");
+    //return 0;
 }
